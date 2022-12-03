8784ec7124ba0b6dfed21fa3490477a129077922
nyp <git.nasunsaeng@gmail.com>
2022-12-04 05:07:01 +0900

    뭔가404수정중

--- src/config/next/config.js
+++ src/config/next/config.js
@@ -2,4 +2,11 @@ const Config = { CURRENT_ENV: 'lo3ca3l' };
 const mergedConfig = {
     Config,
 };
-module.exports = mergedConfig;
+const isProd = process.env.NODE_ENV === 'production' // 프로덕션 모드인지
+
+module.exports = {
+  ... mergedConfig,
+  assetPrefix: isProd
+    ? 'nsFlix.co.kr'
+    : '',
+}
