c1636bc79b9930e320761469463b057c9d05e307
nyp <git.nasunsaeng@gmail.com>
2022-12-04 05:31:02 +0900

    ...

--- src/config/next/config.js
+++ src/config/next/config.js
@@ -1,12 +1,7 @@
-const Config = { CURRENT_ENV: 'lo3ca3l' };
+const Config = {
+    CURRENT_ENV: 'lo3ca3l'
+};
 const mergedConfig = {
     Config,
 };
-const isProd = process.env.NODE_ENV === 'production' // 프로덕션 모드인지
-
-module.exports = {
-  ... mergedConfig,
-  assetPrefix: isProd
-    ? 'nsFlix.co.kr'
-    : '',
-}
+module.exports = mergedConfig;
